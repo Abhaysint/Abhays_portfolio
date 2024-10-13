a5e0308404948dbea146c2cb049f4ad9bb40acc1
Abhay Shaji Valiyaparambil <127753884+Abhaysint@users.noreply.github.com>
2024-10-13 19:19:42 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:'export',
-    typescript:{
-        ignoreBuildErrors:true,
+    output: 'export',
+    basePath: '/Abhays_portfolio',  // Adjust to your repository name
+    assetPrefix: '/Abhays_portfolio/',  // Ensure assets load properly on GitHub Pages
+    typescript: {
+        ignoreBuildErrors: true,
     },
-    eslint:{
-        ignoreBuildErrors:true,
-        ignoreDuringBuilds:true,
-    }
+    eslint: {
+        ignoreBuildErrors: true,
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        unoptimized: true,  // Disable Next.js image optimization for static exports
+    },
 };
 
 export default nextConfig;
